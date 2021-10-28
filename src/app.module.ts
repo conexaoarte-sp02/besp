5c43d838e5c8121cc30d0abfa8de0dd9aac8b26d
Anthony Rafael <anthonyribeiro1910@gmail.com>
2021-10-27 21:46:09 -0300

    Categories Challenge

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { CategoriesModule } from './categories/categories.module';
 import { PlacesModule } from './places/places.module';
 
 @Module({
-  imports: [PlacesModule],
+  imports: [PlacesModule, CategoriesModule],
 })
 export class AppModule {}
